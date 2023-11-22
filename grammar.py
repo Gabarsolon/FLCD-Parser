364e695ac77f7580f26b1c932f82aa4a4a2f862c
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-11-22 21:03:59 +0200

    LAB-5: Created Grammar class

---
+++ grammar.py
@@ -0,0 +1,16 @@
+class Grammar:
+    def __init__(self):
+        #TODO
+        self.non_terminals = set()
+        self.terminals = set()
+        self.productions = set()
+        return None
+    def read_grammar_from_file(self, file_path):
+        #TODO
+        return None
+    def productions_for_a_given_non_terminal(self, non_terminal):
+        #TODO
+        return None
+    def cfg_check(self):
+        #TODO
+        return None
